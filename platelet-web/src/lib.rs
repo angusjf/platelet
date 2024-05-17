c2a12cc141dc426b40b5cd758a67e8fca83bc124
angusjf <angus.findlay@prima.it>
2024-05-17 18:03:50 +0100

    re-enable workflow

--- platelet-web/src/lib.rs
+++ platelet-web/src/lib.rs
@@ -1,11 +1,31 @@
 use std::collections::HashMap;
 
+use serde_json::Value;
 use wasm_bindgen::prelude::wasm_bindgen;
+
+use platelet::renderer::Filesystem;
+
+struct FileMap {
+    files: HashMap<String, String>,
+}
+
+impl Filesystem for FileMap {
+    fn move_to(&self, _current: &String, path: &String) -> String {
+        path.to_owned()
+    }
+
+    fn read(&self, file: &String) -> String {
+        self.files.get(file).unwrap().to_owned()
+    }
+}
 
 #[wasm_bindgen]
 pub fn render_files(filename: String, files: String, context: String) -> Result<String, String> {
-    // let context: Value = serde_json::from_str(&context).unwrap();
+    let context: Value = serde_json::from_str(&context).unwrap();
 
-    // platelet::render_to_string(&context, html).map_err(|e| e.to_string())
-    Ok(String::new())
+    let filesystem: HashMap<String, String> = serde_json::from_str(&files).unwrap();
+
+    let filesystem = FileMap { files: filesystem };
+
+    platelet::render_to_string(&context, &filename, &filesystem).map_err(|e| e.to_string())
 }
