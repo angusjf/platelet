1a972b4dcb0506915f4aab1a7cb6188d3c921901
angusjf <angus.findlay@prima.it>
2024-05-20 16:19:49 +0100

    bring back errors

--- platelet-web/web/playground/worker.js
+++ platelet-web/web/playground/worker.js
@@ -2,6 +2,13 @@ self.onmessage = async function (evt) {
   let data = evt.data;
   let { default: init, render_files } = await import("https://angusjf.com/platelet/pkg/platelet_web.js");
   await init();
-  let result = render_files(data.root, JSON.stringify(data.files), data.json);
-  postMessage(result);
+  try {
+    let result = render_files(data.root, JSON.stringify(data.files), data.json);
+    postMessage({ ok: true, result });
+  } catch (error) {
+    console.error(error);
+    postMessage({ ok: false, error: error.toString() });
+  }
 };
+
+postMessage({ ok: true, result: "loaded!" });
