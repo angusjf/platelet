db38a572cb7f19cd0e22cff1f1ccd1c58af3f3e6
angusjf <angus.findlay@prima.it>
2024-05-20 19:45:28 +0100

    cache import in worker

--- platelet-web/web/playground/worker.js
+++ platelet-web/web/playground/worker.js
@@ -1,7 +1,10 @@
+let render_files;
+
 self.onmessage = async function (evt) {
   let data = evt.data;
-  let { default: init, render_files } = await import("https://angusjf.com/platelet/pkg/platelet_web.js");
-  await init();
+  if (render_files) {
+    render_files = await import("https://angusjf.com/platelet/pkg/platelet_web.js").render_files;
+  }
   try {
     let result = render_files(data.root, JSON.stringify(data.files), data.json);
     postMessage({ ok: true, result });
