486370308b929368ee5919c4c040a4bb45193d0e
angusjf <angus.findlay@prima.it>
2024-05-21 09:58:18 +0100

    move all wasm calls into worker

--- platelet-web/web/playground/worker.js
+++ platelet-web/web/playground/worker.js
@@ -3,7 +3,10 @@ let render_files;
 self.onmessage = async function (evt) {
   let data = evt.data;
   if (!render_files) {
-    render_files = (await import("https://angusjf.com/platelet/pkg/platelet_web.js")).render_files;
+    let platelet = await import("https://angusjf.com/platelet/pkg/platelet_web.js");
+    await platelet.default();
+    console.log(render_files);
+    render_files = platelet.render_files;
   }
   try {
     let result = render_files(data.root, JSON.stringify(data.files), data.json);
