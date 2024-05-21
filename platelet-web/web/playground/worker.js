68960a160a190b261513634ddcfa0fdb8615368c
angusjf <angus.findlay@prima.it>
2024-05-21 09:20:33 +0100

    missing brackets around await

--- platelet-web/web/playground/worker.js
+++ platelet-web/web/playground/worker.js
@@ -3,7 +3,7 @@ let render_files;
 self.onmessage = async function (evt) {
   let data = evt.data;
   if (!render_files) {
-    render_files = await import("https://angusjf.com/platelet/pkg/platelet_web.js").render_files;
+    render_files = (await import("https://angusjf.com/platelet/pkg/platelet_web.js")).render_files;
   }
   try {
     let result = render_files(data.root, JSON.stringify(data.files), data.json);
